72d15c0e184ab6e729fe4d1ec38d3c511b16fee7
Brett Knowles <bkaudev@yahoo.com>
2021-11-06 16:01:55 -0500

    Create file for JS, HTML, and CSS. Write code for each and complete day 1 of '100 Days of Code'

---
+++ 100daysofcode.js
@@ -0,0 +1,8 @@
+/* Array Methods in JavaScript*/
+
+const movies = ["Titanic", "No Country for Old Men", "The Lighthouse", "Scott Pilgrim vs. The World", "Nacho Libre"];
+
+document.getElementById("movies1").innerHTML = movies.unshift("Let the Right One In");
+document.getElementByID("movies2").innerHTML = movies.unshift;
+
+documents.getElementByID("movies3").innerHTML = movies.push("Saving Private Ryan");
